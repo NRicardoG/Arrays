3fcb1ecbdbdcf7a2fe82a75ea2dc4741a7b205d0
DREYGON21 <83626661+NRicardoG@users.noreply.github.com>
2023-04-01 00:36:25 +0000

    aprendiendo direccionamiento

--- arrays.js/desestructuras/jsIndex.js
+++ arrays.js/desestructuras/jsIndex.js
@@ -1,29 +1,30 @@
-let vNombre = new Array();
-function ordenar(){
+let vnombres = new Array(); //[]
+function ordenar() {
 
     let nombre = document.miformulario.nombre.value;
-    vNombre.push(nombre);
-    vNombre.sort();
-    document.miformulario.ordenados.value = vNombre.join("\n");
+
+    vnombres.push(nombre);
+    vnombres.sort();
+
+    document.miformulario.ordenados.value = vnombres.join("\n");
 
     document.miformulario.nombre.value = "";
 }
 
-function ordenar2(){
+function ordenar2() {
 
     let nombre = document.miformulario.nombre.value;
-    
-    let vnombre2 = document.miformulario.ordenados.value.split("\n");
-    vnombre2.push(nombre);
-    vnombre2.sort();
 
-    document.miformulario.ordenados.value = vNombre.join("\n");
+    let vnombres2 = document.miformulario.ordenados.value.split("\n");
+    vnombres2.push(nombre);
+    vnombres2.sort();
+
+    document.miformulario.ordenados.value = vnombres2.join("\n");
 
     document.miformulario.nombre.value = "";
-
 }
 
-
-function limpiarTextArea(){
+function flimpiarTxtArea() {
     document.miformulario.ordenados.value = "";
 }
+
